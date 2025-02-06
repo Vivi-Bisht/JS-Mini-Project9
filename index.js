5aeb9643f914fdcd7d76153817fc777ff7f08809
Vivi-Bisht <166357465+Vivi-Bisht@users.noreply.github.com>
2025-02-06 08:23:20 +0530

    Added JavaScript

---
+++ index.js
@@ -0,0 +1,27 @@
+let lists = document.querySelectorAll(".list");
+let leftBox = document.querySelector("#left");
+let rightBox = document.querySelector("#right");
+
+for(list of lists){
+    list.addEventListener("dragstart" , function(e){
+        let selected = e.target;
+
+        rightBox.addEventListener("dragover" , function(e){
+            e.preventDefault();
+        })
+
+        rightBox.addEventListener("drop" , function(e){
+            rightBox.appendChild(selected);
+            selected = null;
+        })
+
+        leftBox.addEventListener("dragover" , function(e){
+            e.preventDefault();
+        })
+
+        leftBox.addEventListener("drop" , function(e){
+            leftBox.appendChild(selected);
+            selected = null;
+        })
+    })
+}
